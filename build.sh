afada8904be54b86dad99f2f17045c07ec5b90b2
Adetunji Peter <tunjipete@grrnberry.com>
2024-11-02 03:24:58 +0100

    get it

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "building the project"
-python3 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "make migration.."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.0 manage.py migrate --noinput
 
 echo "collect static..."
-python3 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
